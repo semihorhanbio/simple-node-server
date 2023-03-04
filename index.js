3e4e210d9fcb0dcba74d205a5aac18576f75047e
Semih <50242428+semihorhanbio@users.noreply.github.com>
2023-03-04 13:26:53 +0000

    edit for repl.it demo

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const path = require('path');
 const fs = require('fs');
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 8080;
-
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.writeHead(200, { 'content-type': 'text/html' })
@@ -25,6 +22,6 @@ const server = http.createServer((req, res) => {
     }
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(8080, () => {
+  console.log('Listeninig on port 8080.....');
 });
